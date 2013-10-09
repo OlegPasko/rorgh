8b9d4c6a9c47550dd076756534ab7505c90fa9c9
Oleg Pasko <olegpasko@gmail.com>
2013-10-10 00:31:59 +0300

    gravatar added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  
+  def gravatar_url(user, size = 100)
+    gravatar_id = Digest::MD5::hexdigest(user.email)
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=identicon"
+  end
+  
 end
